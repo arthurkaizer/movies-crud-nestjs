e1879f5199c705d9ad31d819e66418c39976b824
arthurkaizer <arturkaizer@gmail.com>
2024-05-23 19:39:02 -0300

    redis configuration and docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PassportModule } from '@nestjs/passport';
 import { AuthModule } from './modules/auth/auth.module';
 import { TypeOrmModule } from './infra/typeorm.module';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     TypeOrmModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     AuthModule,
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
